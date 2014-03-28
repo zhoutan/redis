2c109abde042b3017186db9ec4a42b2cb1580a81
Matt Stancliff <matt@genges.com>
2014-03-27 22:41:03 -0400

    Add module type definition
    We may have different types of modules in the future
    instead of only loadable commands.  Require all modules to tell
    us their purpose.
    
    Effect of defining a module as type REDIS_MODULE_COMMAND:
    we load redisCommandTable[] from the module.

--- src/redis.h
+++ src/redis.h
@@ -864,7 +864,10 @@ struct redisCommandsTable {
     int length;
 };
 
+#define REDIS_MODULE_COMMAND 1
+
 struct redisModule {
+    int type;
     char *version;
     char *name;
     void *(*load)(void);
@@ -880,6 +883,7 @@ struct redisModuleInfo {
     void *privdata;
     time_t loaded_first;
     time_t loaded_last;
+    int type;
 };
 
 struct redisFunctionSym {
