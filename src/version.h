9a1a60293cea523b354d9ae43438eb19f3060de8
Matt Stancliff <matt@genges.com>
2014-04-17 11:04:09 -0400

    Dynamic Redis 2.8.8-0.5.1
    Changes:
      - export some internal Redis functiosns so we can use them in modules
      - Add dynamic redis version define for conditionally compiling module features

--- src/version.h
+++ src/version.h
@@ -1,1 +1,4 @@
-#define REDIS_VERSION "2.8.8-dynamic-0.5"
+#define REDIS_VERSION "2.8.8-dynamic-0.5.1"
+/* VER is: prefix of 1, then:
+ * zero-padded two digit number for each version decimal */
+#define DYN_REDIS_VER 1000501
