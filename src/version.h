785b86215669d82bf4ff4f52bdc760267391d15d
Matt Stancliff <matt@genges.com>
2014-04-22 11:07:12 -0400

    Fix zzlFirstInRange prototype
    The implementation changed with the implementation of ZRANGEBYLEX
    into now accepting a pointer instead of the struct directly.

--- src/version.h
+++ src/version.h
@@ -1,1 +1,6 @@
-#define REDIS_VERSION "2.8.19"
+#define REDIS_VERSION "2.8.19-dynamic-0.5.1"
+/* VER is: prefix of 1, then:
+ * zero-padded two digit number for each version decimal */
+/* Remove DYN_REDIS_VER for now because... it was only guarding
+ * one feature change not relevant in this release. */
+/* #define DYN_REDIS_VER 1000501 */
