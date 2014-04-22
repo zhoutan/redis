51e7ef2c68779fd55d81f381a637fc2ffd57235f
Matt Stancliff <matt@genges.com>
2014-04-22 11:20:11 -0400

    Add coarse feature flags for compile-time use
    Some functions and features of modules need to be disabled
    based on the presence or absense of things.  Let's try feature
    flags instead of using floating versions.

--- src/version.h
+++ src/version.h
@@ -4,3 +4,6 @@
 /* Remove DYN_REDIS_VER for now because... it was only guarding
  * one feature change not relevant in this release. */
 /* #define DYN_REDIS_VER 1000501 */
+
+#define DYN_FEATURE_CLUSTER false
+#define DYN_FEATURE_EMBSTR false
