89b8d5935a129941eff548d1df10696915533e7c
Matt Stancliff <matt@genges.com>
2014-05-09 12:06:06 -0400

    Convert check-dump to Redis check-rdb mode
    redis-check-dump is now named redis-check-rdb and it runs
    as a mode of redis-server instead of an independent binary.
    
    You can now use 'redis-server redis.conf --check-rdb' to check
    the RDB defined in redis.conf.  Using argument --check-rdb
    checks the RDB and exits.  We could potentially also allow
    the server to continue starting if the RDB check succeeds.
    
    This change also enables us to use RDB checking programatically
    from inside Redis for certain failure conditions.

--- src/redis.h
+++ src/redis.h
@@ -1361,6 +1361,9 @@ void sentinelTimer(void);
 char *sentinelHandleConfiguration(char **argv, int argc);
 void sentinelIsRunning(void);
 
+/* redis-check-rdb */
+int redis_check_rdb(char *rdbfilename);
+
 /* Scripting */
 void scriptingInit(void);
 
