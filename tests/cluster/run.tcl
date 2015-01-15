989bf2ed6f1a9f4117021b269381685d407737ad
Matt Stancliff <matt@genges.com>
2015-01-15 11:26:43 -0500

    Lower number of test cluster nodes
    Under valgrind, each cluster node is 200+ MB, so we can't run
    too many of them on small CI servers.

--- tests/cluster/run.tcl
+++ tests/cluster/run.tcl
@@ -7,7 +7,7 @@ source cluster.tcl
 source ../instances.tcl
 source ../../support/cluster.tcl ; # Redis Cluster client.
 
-set ::instances_count 20 ; # How many instances we use at max.
+set ::instances_count 12 ; # How many instances we use at max.
 
 proc main {} {
     parse_options
